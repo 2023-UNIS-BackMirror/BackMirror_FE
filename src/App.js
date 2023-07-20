e8040d2f2b8fd649beef7d9fab21a1717be6fca3
jimin song <songing01@gmail.com>
2023-07-20 15:05:37 +0900

    ✨ Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import QnAPage from "./pages/QnAPage";
+
 function App() {
-  return <div>BackMirror</div>;
+  return (
+    <Routes>
+      <Route path="/QnA" element={<QnAPage />} />
+    </Routes>
+  );
 }
 
 export default App;
