cc7635d433408ed121b718527f1bc2acd1c2c97d
bbandm <134187969+bbandm@users.noreply.github.com>
2023-07-20 19:56:16 +0900

    Add files via upload
    KakaoLogin

---
+++ src/components/KakaoLogin/KakaoLogin.stories.js
@@ -0,0 +1,12 @@
+import { KakaoLogin } from ".";
+
+export default {
+  title: "Components/KakaoLogin",
+  component: KakaoLogin,
+};
+
+export const Default = {
+  args: {
+    className: {},
+  },
+};
