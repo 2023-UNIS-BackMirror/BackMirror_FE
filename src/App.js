820c962540424d911288a9571ab0c8990559e25e
bbandm <134187969+bbandm@users.noreply.github.com>
2023-07-21 01:55:02 +0900

    Update App.js
    goHome goList 루트설정 완료

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import QnACompletePage from "./pages/QnACompletePage copy";
 import FailuresPage from "./pages/FailuresPage";
 import FailureDetailPage from "./pages/FailureDetailPage";
 import PromoPage from "./pages/PromoPage";
+import HomeScreen from "./pages/HomeScreen" ;
+import FailListScreen from "./pages/FailListScreen"
 function App() {
   return (
     <Routes>
@@ -14,6 +16,8 @@ function App() {
       <Route path="/failures" element={<FailuresPage />} />
       <Route path="/failures/:id" element={<FailureDetailPage />} />
       <Route path="/promotion" element={<PromoPage />} />
+      <Route path="/goHome" element={<HomeScreen/>}/>
+      <Route path="/goList" element={<FailListScreen/>}/>
     </Routes>
   );
 }
