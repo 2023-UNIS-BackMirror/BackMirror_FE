d0997f23078040e4a36f6c5da4fdc6d3bc413a14
jimin song <songing01@gmail.com>
2023-07-21 01:22:43 +0900

    🐛 루트 수정

--- src/api/client.js
+++ src/api/client.js
@@ -5,9 +5,8 @@ const client = axios.create();
 client.defaults.baseURL = `${SPRING_URL}`;
 client.defaults.withCredentials = true;
 
-//const token = localStorage.getItem("token");
-const token =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNCIsImF1dGgiOiIxNCIsInR5cGUiOiJhY2Nlc3MiLCJpYXQiOjE2ODk4NDg1MzcsImV4cCI6MTY4OTkzNDkzN30.d5e9ntr5haVYWRxZAGgXCLBr0YslauumKyD1DE_Pgl1H7WXH3zHtAa9aVBDgz7HrZnWC9wl--g6Xf_JgSfuGiQ";
+const token = localStorage.getItem("token");
+
 console.log("현재 토큰", token);
 
 client.defaults.headers.common["Authorization"] = token ? `${token}` : null;
